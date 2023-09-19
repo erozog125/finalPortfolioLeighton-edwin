be22edc5d4e24c90d549981fc952143d0250e7b8
Edwin Rozo Gómez <53238539+erozog125@users.noreply.github.com>
2023-09-19 08:51:29 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Navbar from "./components/Navbar/Navbar";
 import Presentation from "./components/Presentation/Presentation";
 import Footer from "./components/Footer/Footer"
 import Languages from "./components/Languages/Languages"
-import "./app.css"
 
 function App() {
   return (
